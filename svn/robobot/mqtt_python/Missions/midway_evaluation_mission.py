c917eab273f90c2f817894d811e8c3d5938d7005
Mup3nda <mupendadidier@gmail.com>
2026-04-29 12:53:32 +0200

    Refactor mission objectives: uncomment and organize DriveTurnAngleObjective and related actions in midway evaluation mission

--- svn/robobot/mqtt_python/Missions/midway_evaluation_mission.py
+++ svn/robobot/mqtt_python/Missions/midway_evaluation_mission.py
@@ -225,76 +225,102 @@ def build_objectives():
         # DelayObjective(1.0),
         # ArmUpObjective(),
 
-    # DRIVE FROM THE LINE TO THE TIMER
-        #ArmUpObjective(),
-        #GripperOpenObjective(),
-        #DriveBackwardUntilLineStopObjective(
-        #      reverse_speed=-0.2,
-        #      line_found_confidence=2,
-        #      timeout_s=8.0,
-        #      max_distance_m=1.5,
-        #  ), 
-        # ResetOriginObjective(), # new 0,0,0 origin after following line end
-        # DelayObjective(1.0),
-        #DriveDistanceObjective(target_distance_m=2.0),
-        # # DriveToTimerAndBackObjective(target_distance = 2.0),
-        # DriveToWaypointObjective(
-        #     waypoint=(2.2, 0.10),
-        #     is_local=False,
-        #     print_interval=20,
-        #     relative_heading_deg=0.0,
-        #     nav_mode=WAYPOINT_NAV_MODE,
-        # ),
-        # DriveToWaypointObjective(
-        #     waypoint=(1.8, 1.5),
-        #     is_local=False,
-        #     print_interval=20,
-        #     relative_heading_deg=-20.0,
-        #     nav_mode=WAYPOINT_NAV_MODE,
-        # ),
-        # DelayObjective(1.0),
-        # SearchAndNavigateToPlatform(marker_id=5, turn_rate=0.18),
+    # ##DRIVE FROM THE LINE TO THE TIMER
+    #     ArmUpObjective(),
+    #     GripperOpenObjective(),
+    #     DriveBackwardUntilLineStopObjective(
+    #          reverse_speed=-0.2,
+    #          line_found_confidence=2,
+    #          timeout_s=8.0,
+    #          max_distance_m=1.5,
+    #      ), 
+    #     ResetOriginObjective(), # new 0,0,0 origin after following line end
+    #     DelayObjective(1.0),
+    #     DriveDistanceObjective(target_distance_m=2.0),
+    #     # DriveToTimerAndBackObjective(target_distance = 2.0),
+    #     DriveToWaypointObjective(
+    #         waypoint=(2.2, 0.10),
+    #         is_local=False,
+    #         print_interval=20,
+    #         relative_heading_deg=0.0,
+    #         nav_mode=WAYPOINT_NAV_MODE,
+    #     ),
+    #     DriveToWaypointObjective(
+    #         waypoint=(1.8, 1.5),
+    #         is_local=False,
+    #         print_interval=20,
+    #         relative_heading_deg=-20.0,
+    #         nav_mode=WAYPOINT_NAV_MODE,
+    #     ),
+    #     DelayObjective(1.0),
+    #     SearchAndNavigateToPlatform(marker_id=5, turn_rate=0.18),
    
-        ########## DETECT ARUCO PLATFORM 
-        DriveTurnAngleObjective(
-            angle_deg=30.0,
-            linear_cmd=0.0,
-            timeout_s=6.0,
-        ),
-        ArmUpObjective(),
-        GripperOpenObjective(),
-        SearchAndNavigateToPlatform(marker_id=5),
-        ArmMiddleObjective(),
-        DelayObjective(2.5),
-        GripperMiddleObjective(),
-        DriveTurnAngleObjective(
-            angle_deg=120.0,
-            linear_cmd=0.0,
-            timeout_s=6.0,
-        ),
-        DelayObjective(1.0),
-        ArmUpObjective(),
-        DriveToWaypointObjective(
-            waypoint=(0.8, 0),
-            is_local=True,
-            print_interval=20,
-            relative_heading_deg=180.0, #180
-            nav_mode=WAYPOINT_NAV_MODE,
-        ),
+    #     ########## DETECT ARUCO PLATFORM 
+    #     DriveTurnAngleObjective(
+    #         angle_deg=30.0,
+    #         linear_cmd=0.0,
+    #         timeout_s=6.0,
+    #     ),
+    #     ArmUpObjective(),
+    #     GripperOpenObjective(),
+    #     SearchAndNavigateToPlatform(marker_id=5),
+    #     ArmMiddleObjective(),
+    #     DelayObjective(2.5),
+    #     GripperMiddleObjective(),
+    #     DriveTurnAngleObjective(
+    #         angle_deg=120.0,
+    #         linear_cmd=0.0,
+    #         timeout_s=6.0,
+    #     ),
+    #     DelayObjective(1.0),
+    #     ArmUpObjective(),
+    #     DriveToWaypointObjective(
+    #         waypoint=(0.8, 0),
+    #         is_local=True,
+    #         print_interval=20,
+    #         relative_heading_deg=180.0, #180
+    #         nav_mode=WAYPOINT_NAV_MODE,
+    #     ),
         
-        ###### LOOKING FOR CUBE AFTER KCOKING
+        # ###### LOOKING FOR CUBE AFTER KCOKING
+        # GripperOpenObjective(),
+        # #DriveDistanceObjective(target_distance_m=0.010),
+        # DelayObjective(1.0),
+        # SearchAndNavigateToAruco(marker_id=53, fallback_marker_id=20, turn_rate=0.3, search_timeout_s = 10.0, desired_distance=0.42, max_sweep_deg=90), #37
+        # CheckNegativeVelocityObjective(),  # End mission immediately if navigation saw negative x velocity
+        # CheckFallbackObjective(),
+        # ArmDownObjective(wait_after_s=2.0),
+        # DriveDistanceObjective(target_distance_m=0.005),
+        # DelayObjective(1.0),
+        # GripperMiddleObjective(),
+        # DelayObjective(1.0),
+        # ConditionalArmObjective(),  # Arm UP if fallback used, ARM MIDDLE if primary used
+        
         GripperOpenObjective(),
-        #DriveDistanceObjective(target_distance_m=0.010),
-        DelayObjective(1.0),
-        SearchAndNavigateToAruco(marker_id=53, fallback_marker_id=20, turn_rate=0.3, search_timeout_s = 10.0, desired_distance=0.42, max_sweep_deg=90), #37
-        CheckNegativeVelocityObjective(),  # End mission immediately if navigation saw negative x velocity
-        CheckFallbackObjective(),
-        ArmDownObjective(wait_after_s=2.0),
-        DriveDistanceObjective(target_distance_m=0.005),
-        DelayObjective(1.0),
-        GripperMiddleObjective(),
-        DelayObjective(1.0),
-        ConditionalArmObjective(),  # Arm UP if fallback used, ARM MIDDLE if primary used
+        DriveDistanceObjective(target_distance_m=10.10),
+        # GripperOpenObjective(),
+        # ArmUpObjective(),
+        # DelayObjective(2.0),
+        # ArmDownObjective(),
+        # DelayObjective(2.0),
+        # ArmUpObjective(),
+        # DelayObjective(2.0),
+        # ArmMiddleObjective(),
+        # DelayObjective(1.0),
+        # GripperOpenObjective(),
+        # DelayObjective(1.0),
+        # GripperMiddleObjective(),
+        # DelayObjective(1.0),
+        # GripperOpenObjective(),
+        # DelayObjective(1.0),
+        # GripperMiddleObjective(),
+        # DelayObjective(1.0),
+        # GripperOpenObjective(),
+        # DelayObjective(1.0),
+        # GripperMiddleObjective(),
+        # DelayObjective(2.0),
+        # ArmUpObjective(),
+        # DelayObjective(1.0),
         ]
     return objectives
 
