0cf5742ee0750a1a5e043a1fc69fc5d8eb3e020c
Mup3nda <mupendadidier@gmail.com>
2026-05-01 07:47:34 +0200

    Refactor mission_start.bash to comment out test commands and clarify final mission execution

--- svn/robobot/setup/mission_start.bash
+++ svn/robobot/setup/mission_start.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 # echo -e "\nStarting mission\n"
-cd /home/local/svn/robobot/mqtt_python
-/usr/bin/python3 mqtt-client.py -n >>log_out.txt 2>>log_err.txt &
+# ----------START THE TEST----------------
+# cd /home/local/svn/robobot/mqtt_python
+# /usr/bin/python3 mqtt-client.py -n >>log_out.txt 2>>log_err.txt &
+
+# -----------START OUR FINAL MISSION--------------
+/usr/bin/python3 -u /home/local/svn/robobot/mqtt_python/Missions/final_competition_mission.py >>log_out.txt 2>>log_err.txt &
+
 # echo "mission ended"
 exit 0
